972e8cc27d3685d315b7c4e8ed94c487c0d58c3a
Hasan Lashgari <hasanlashgari01@gmail.com>
2023-08-08 21:56:00 +0330

    fix: fix find user model sort by descending

--- controllers/infos.js
+++ controllers/infos.js
@@ -7,7 +7,7 @@ exports.getInfos = async (req, res, next) => {
         const productsCount = await productModel.find().lean().count();
         const usersCount = await userModel.find().lean().count();
         const commentsCount = await commentModel.find().lean().count();
-        const users = await userModel.find().sort("-1").lean();
+        const users = await userModel.find().sort({ _id: -1 }).lean();
 
         res.json({ productsCount, usersCount, commentsCount, users });
     } catch (err) {
