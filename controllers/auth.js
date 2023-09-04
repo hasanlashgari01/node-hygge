33d6ed64b835a76f54c58de0d975d379e9b31ece
Hasan Lashgari <hasanlashgari01@gmail.com>
2023-09-04 16:15:46 +0330

    require path

--- controllers/auth.js
+++ controllers/auth.js
@@ -71,14 +71,11 @@ exports.getMe = async (req, res, next) => {
     try {
         const decoded = jwt.verify(token, process.env.SECRET_KEY);
 
-        console.log(decoded);
-
         const user = await userModel
             .findOne({ email: decoded.email }, { password: 0, __v: 0 })
             .populate("likes", "-__v -category")
             .populate("bookmarks", "-__v -category");
 
-        // console.log(user);
         if (!user) {
             return res.status(404).json({ message: "User Not Found" });
         }
