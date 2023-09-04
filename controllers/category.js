3d322f678c13668f1fafe355ca8d1eacb6cdeae4
Hasan Lashgari <hasanlashgari01@gmail.com>
2023-09-04 16:15:46 +0330

    require path

--- controllers/category.js
+++ controllers/category.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const { isValidObjectId } = require("mongoose");
 const { categoryValidatationSchema } = require("../validators/categoriesValidator");
 const categoriesModel = require("../models/category");
